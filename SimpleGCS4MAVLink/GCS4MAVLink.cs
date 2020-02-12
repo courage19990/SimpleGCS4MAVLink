69f1074e39748c8dfc92f6f59776a3bd4c91685e
courage19990 <2625665755@qq.com>
2020-02-12 19:56:48 +0800

    Add project files.

---
+++ SimpleGCS4MAVLink/GCS4MAVLink.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleGCS4MAVLink
+{
+    public partial class GCS4MAVLink : Form
+    {
+        public GCS4MAVLink()
+        {
+            InitializeComponent();
+        }
+    }
+}
